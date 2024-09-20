b58a38c7cdbcf06b63bfc15e55e894a391133ff5
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-09-20 20:58:10 +0530

    file handling

---
+++ File Handling/file2.py
@@ -0,0 +1,14 @@
+# f5=open("file3.txt","w")
+# f5.write("File Handling in python")
+# f5.close()
+# f5=open("file3.txt","a")
+# f5.write("\nFile Handling in python to store data and code as it is ")
+# f5.close()
+
+
+f6 = open("file3.txt","w")
+f6.write("Live life without worries")
+f6.close()
+f6=open("file3.txt","a")
+f6.write("\nzindagi ek safar hai suhana yha kal kya ho kisne jaana ")
+f6.close()
